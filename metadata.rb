9ec763ca5c05f989fd314bc4e7ba29a62b90bc47
Lei He <lei.he@blackboard.com>
2016-03-16 13:32:22 +0800

    Initial.

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name 'cloud_learn'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures cloud_learn'
+long_description 'Installs/Configures cloud_learn'
+version '1.15.0'
+
+depends 'newrelic', '~> 2.14.0'
